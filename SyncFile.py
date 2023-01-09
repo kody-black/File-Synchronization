3b0e4af4bca4c10a6896a35cc5ef0c21f5f7c734
distiny-cool <2442658787@qq.com>
2023-01-10 00:30:38 +0800

    v 1.0

--- SyncFile.py
+++ SyncFile.py
@@ -1,14 +1,18 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QMessageBox, QFileDialog, QTextEdit
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QDialog, QLabel, QLineEdit, QPushButton, QMessageBox, QFileDialog, QTextEdit, QHBoxLayout, QVBoxLayout, QWhatsThis
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import QEvent
 import shutil
 import os
 import sys
 import ctypes
 
-class SyncFilesWindow(QWidget):
+class SyncFilesWindow(QDialog):
     def __init__(self):
         super().__init__()
-        self.setWindowIcon(QIcon("./icon.ico"))
+        filename = self.resource_path(os.path.join("ico","icon.ico"))
+        icon = QIcon()
+        icon.addPixmap(QPixmap(filename), QIcon.Normal, QIcon.Off)
+        self.setWindowIcon(icon)
         ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("myappid")
 
         # 创建文本标签和文本框
@@ -19,21 +23,31 @@ class SyncFilesWindow(QWidget):
         self.dst_edit = QLineEdit(self)
         self.dst_button = QPushButton("选择文件夹", self)
 
-        # 创建文本框
+        # 创建文本框用于显示同步的文件名称
         self.log_edit = QTextEdit(self)
 
         # 创建按钮
         self.sync_button = QPushButton("同步文件", self)
 
+        # 创建水平布局，用于放置文本框和按钮
+        hbox_src = QHBoxLayout()
+        hbox_src.addWidget(self.src_edit)
+        hbox_src.addWidget(self.src_button)
+        hbox_dst = QHBoxLayout()
+        hbox_dst.addWidget(self.dst_edit)
+        hbox_dst.addWidget(self.dst_button)
+
+        # 创建垂直布局
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.src_label)
+        vbox.addLayout(hbox_src)
+        vbox.addWidget(self.dst_label)
+        vbox.addLayout(hbox_dst)
+        vbox.addWidget(self.sync_button)
+        vbox.addWidget(self.log_edit)
+
         # 设置布局
-        self.src_label.move(10, 10)
-        self.src_edit.move(130, 10)
-        self.src_button.move(315, 10)
-        self.dst_label.move(10, 40)
-        self.dst_edit.move(130, 40)
-        self.dst_button.move(315, 40)
-        self.log_edit.setGeometry(10, 70, 400, 150)
-        self.sync_button.move(160, 230)
+        self.setLayout(vbox)
 
         # 连接按钮的点击事件和处理函数
         self.src_button.clicked.connect(self.select_src_dir)
@@ -41,10 +55,25 @@ class SyncFilesWindow(QWidget):
         self.sync_button.clicked.connect(self.sync_files)
 
         # 设置窗口的属性
-        self.setGeometry(300, 300, 420, 270)
+        self.setGeometry(300, 300, 420, 420)
         self.setWindowTitle('文件夹同步工具')
 
         self.infomation()
+
+    def event(self, event):
+        if event.type()==QEvent.EnterWhatsThisMode:
+            QWhatsThis.leaveWhatsThisMode()
+            QMessageBox.information(self, "关于", "开发：Kody Black\n"+
+            "github.com/distiny-cool/File-Synchronization")
+        return QDialog.event(self,event)
+
+
+    def resource_path(self, relative_path):
+        if getattr(sys, 'frozen', False): 
+            base_path = sys._MEIPASS
+        else:
+            base_path = os.path.abspath(".")
+        return os.path.join(base_path, relative_path)
 
     def select_src_dir(self):
         # 调用 QFileDialog.getExistingDirectory() 函数弹出文件夹选择对话框
@@ -61,10 +90,11 @@ class SyncFilesWindow(QWidget):
             self.dst_edit.setText(dst_dir)
 
     def infomation(self):
-        self.log_edit.append('<font color=\"#d83b01\"><b>注意：</b></font>'+'<p>'+
-        '本软件的作用是把源文件夹的内容同步到目标文件夹中'+'<p>'+
-        '程序会遍历源文件夹，把相对于目标文件夹不同的地方（包括修改和添加的文件）复制到目标文件夹（其中修改过的文件会覆盖掉目标文件）'+'<p>'+
-        '<font color=\"#0078d4\"><b>目标文件夹不允许是源文件夹的子文件夹</b></font>')
+        self.log_edit.append('<font color="#0078d4"><b>功能介绍：</b></font>'+'<p>'+
+        '本软件会把源文件夹和目标文件夹中不同的内容复制到目标文件夹里，完全相同的文件则直接跳过，以节省你的复制时间'+'<p>'+
+        '<font color="#d83b01"><b>注意：</b></font><ul><li>目标文件夹不允许是源文件夹的子文件夹</li>'+
+        '<li>如果有相同的文件，则在目标文件夹中会保留最近修改过的文件</li></ul>'+
+        '<p style="text-align: right;"><font color="#6e9bc5">'+'<b> by:Kody </b></font></p>')
 
     def sync_files(self):
         # 获取文本框中的路径
